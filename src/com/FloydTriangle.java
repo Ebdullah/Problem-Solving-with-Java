a50b3967c10e78e787c2b06e966883a1c09a2da6
Ebdullah <sandhu.ch66@gmail.com>
2025-02-11 10:34:50 -0500

    Initial commit for Floyd's Triangle project

---
+++ src/com/FloydTriangle.java
@@ -0,0 +1,17 @@
+package com;
+
+public class FloydTriangle {
+
+    public void floydTriangle() {
+        int n = 5;
+        int number = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
+
---
+++ src/com/Main.java
@@ -0,0 +1,8 @@
+package com;
+
+public class Main {
+    public static void main(String[] args) {
+        FloydTriangle fT = new FloydTriangle();
+        fT.floydTriangle();
+    }
+}
